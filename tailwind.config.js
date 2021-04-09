55024a7cdc85729e597509eaf27dcce0404db73a
Iramboana-Thierry <irambonat0@gmail.com>
2021-04-09 17:48:58 +0200

    completed side bar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        head: '#1a202c',
+        dim: '#b7c9fa',
+        dip: '#2e3748'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
